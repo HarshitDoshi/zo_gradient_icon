0731e7a93afc1d01f8168898f8e056ac80d5a992
Harshit Doshi <harshitdoshi719@gmail.com>
2019-11-08 09:53:16 +0530

    initial commit...

---
+++ lib/zo_gradient_icon.dart
@@ -0,0 +1,42 @@
+library zo_gradient_icon;
+
+import 'package:flutter/material.dart';
+
+class ZOGradientIcon extends StatefulWidget {
+  ZOGradientIcon(
+      this.icon,
+      this.size,
+      this.gradientColorsList,
+      );
+
+  final IconData icon;
+  final double size;
+  final List<Color> gradientColorsList;
+
+  @override
+  _ZOGradientIconState createState() => _ZOGradientIconState();
+}
+
+class _ZOGradientIconState extends State<ZOGradientIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ShaderMask(
+        shaderCallback: (Rect bounds) {
+          return LinearGradient(
+            colors: widget.gradientColorsList,
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+            stops: [0.0, 0.33, 0.66, 1.0],
+            tileMode: TileMode.mirror,
+          ).createShader(bounds);
+        },
+        child: Icon(
+          widget.icon,
+          color: Colors.white,
+          size: widget.size,
+        ),
+      ),
+    );
+  }
+}
